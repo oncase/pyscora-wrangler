ba674cf242956ed319569abb05f50ac4aade6ee9
Adriel Martins <adrielfalcao@gmail.com>
2021-04-19 12:44:35 -0300

    Adjusting code line lengths

--- pyscora_wrangler/transform.py
+++ pyscora_wrangler/transform.py
@@ -18,20 +18,22 @@ def to_parquet(
     Write chunks of pandas.DataFrame into a single or multiple parquet files.
 
     Args:
-        stream (Chunk generator or pd.DataFrame): Stream of data through chunks. If it is a 
-            single pd.DataFrame then splits into a chunk list using 'chunksize' argument.
-        outuput_parquet (str): filepath to write the single file, or path to the folder where 
-            to write multiple chunks file.
-        multiple_files (bool, optional): if True, then the 'output_parquet' must be a folder path,
-        otherwise, it must be a file path.
-        print_every (int, optional): Print the chunk index every 'print every'. If None, then it
-            does not print. Defaults to None.
-        mode (str, optional): It can be either 'overwrite' or 'append'. If 'overwrite',
-            deletes folder/file and rewrite it. If 'appends' and 'multiple_files' is True then it writes
-            following the current chunk division of "chunk_0.parquet", "chunk_1.parquet", etc.
-            Defaults to 'overwrite'.
-        chunksize (int, optional): Only used if 'stream' is pd.DataFrame. The chunksize to split the
-        pd.DataFrame. Defaults to None.
+        stream (Chunk generator or pd.DataFrame): Stream of data through chunks.
+        If it is a single pd.DataFrame then splits into a chunk list using
+        'chunksize' argument.
+        outuput_parquet (str): filepath to write the single file, or path to the
+            folder where to write multiple chunks file.
+        multiple_files (bool, optional): if True, then the 'output_parquet' must
+            be a folder path, otherwise, it must be a file path.
+        print_every (int, optional): Print the chunk index every 'print every'.
+            If None, then it does not print. Defaults to None.
+        mode (str, optional): It can be either 'overwrite' or 'append'. If 
+            'overwrite', deletes folder/file and rewrite it. If 'appends' and 
+            'multiple_files' is True then it writes following the current chunk 
+            division of "chunk_0.parquet", "chunk_1.parquet", etc. Defaults to 
+            'overwrite'.
+        chunksize (int, optional): Only used if 'stream' is pd.DataFrame. The 
+        chunksize to split the pd.DataFrame. Defaults to None.
             
     Returns:
         None.
@@ -56,7 +58,8 @@ def to_parquet(
             else:
                 table = pa.Table.from_pandas(chunk, schema=schema)
             pa.parquet.write_table(table,
-                                   where=f"{output_parquet}/chunk_{index}.parquet")
+                                   where=f"{output_parquet}/" + 
+                                   f"chunk_{index}.parquet")
             
     if multiple_files == False:
         for i, chunk in enumerate(stream):
@@ -65,7 +68,8 @@ def to_parquet(
             if i == 0:
                 # Infer schema and open parquet file on first chunk
                 parquet_schema = pa.Table.from_pandas(df=chunk).schema
-                parquet_writer = pq.ParquetWriter(output_parquet, parquet_schema, compression='snappy')
+                parquet_writer = pq.ParquetWriter(output_parquet, parquet_schema
+                                                  ,compression='snappy')
                 
             table = pa.Table.from_pandas(chunk, schema=parquet_schema)
             parquet_writer.write_table(table)
