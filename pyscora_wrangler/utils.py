bf423bde1ef051b9e3622aa9eeadd7636cb1201a
Adriel Martins <adrielfalcao@gmail.com>
2021-08-12 16:11:28 -0300

    feat: adding decoder of files to the pyproject

--- pyscora_wrangler/utils.py
+++ pyscora_wrangler/utils.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from smart_open import open
 
 
 def overwrite_folder(directory):
@@ -11,3 +12,25 @@ def overwrite_folder(directory):
 def make_folder(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+
+def decoder_of_files(
+    input_path: str,
+    output_path: str,
+    dcd_output: str = 'latin',
+) -> None:
+    """It will overwrite the output file.
+
+    Args:
+        input_path (str)
+        output_path (str)
+        dcd_input (str, optional)
+        dcd_output (str, optional)
+    """
+    if input_path == output_path:
+        raise Exception('input_path and output_path must be different.')
+    with open(output_path, 'w') as new_file:
+        with open(input_path, 'rb') as input_file:
+            for line in input_file:
+                line = line.decode(dcd_output)
+                new_file.write(line)
