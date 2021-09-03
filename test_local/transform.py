b71057288e4e8000866ef94c440434c73b308c05
Adriel Martins <adrielfalcao@gmail.com>
2021-09-03 16:38:37 -0300

    refact: cleaning useless files

--- test_local/transform.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from pyscora_wrangler.transform import to_parquet
-
-stream = pd.read_csv('test_data/input/MICRODADOS_ENEM_2019.csv',
-                     delimiter=';',
-                     chunksize=100000)
-to_parquet(stream,
-           output_parquet='test_data/output',
-           multiple_files=True)
