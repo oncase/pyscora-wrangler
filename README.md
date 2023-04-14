24d844ea8109dc8d606735164d9f494f63dd0b9c
Guilherme Morone <guimorone@gmail.com>
2023-04-14 14:58:26 -0300

    New methods in LdapService

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Pyscora Wrangler
 
 <p align="center">
-<img alt="Python versions" src="https://img.shields.io/badge/python-3.8%20%7C%203.9%20%7C%203.10%20%7C%203.11-brightgreen.svg">
+<img alt="Python versions" src="https://img.shields.io/badge/python-3.8%20%7C%203.9%20%7C%203.10-brightgreen.svg">
 <a href="https://github.com/oncase/pyscora-wrangler/blob/main/LICENSE"><img alt="License: MIT" src="https://black.readthedocs.io/en/stable/_static/license.svg"></a>
 <a href="https://github.com/psf/black"><img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg"></a></p>
 
--- pyscora_wrangler/ldap/service/__init__.py
+++ pyscora_wrangler/ldap/service/__init__.py
@@ -43,9 +43,10 @@ class LdapService:
         # Private
         self.__ldap_username: str | None = None
         self.__ldap_password: str | None = None
-        self.__ldap_connection: Connection = None
-        self.__ldap_groups: List[str] = []
-        self.__ldap_users: List[str] = []
+        self.__ldap_connection: Connection | None = None
+        self.__all_ldap_dns: List[str] | None = None
+        self.__all_ldap_groups: List[str] | None = None
+        self.__all_ldap_usernames: List[str] | None = None
         self.__user_is_authenticated: bool = False
 
         ldap_config_is_valid, err_msgs = self.__is_ldap_config_valid()
@@ -60,7 +61,7 @@ class LdapService:
 
         Returns:
             Tuple[bool, List[str]]:
-                - bool: True if `ldap_config` is valid, False otherwise.
+                - bool: `True` if `ldap_config` is valid, `False` otherwise.
                 - List[str]: Error messages
         """
 
@@ -91,7 +92,7 @@ class LdapService:
         """Check if user is authenticated to ldap server
 
         Returns:
-            bool: True if user is authenticated to ldap server, False otherwise.
+            bool: `True` if user is authenticated to ldap server, `False` otherwise.
         """
 
         return self.__user_is_authenticated
@@ -107,7 +108,7 @@ class LdapService:
             ValueError: If `username` or `password` is null or is empty string.
 
         Returns:
-            bool: True if user is authenticated to ldap server, False otherwise.
+            bool: `True` if user is authenticated to ldap server, `False` otherwise.
         """
 
         try:
@@ -153,27 +154,11 @@ class LdapService:
 
         return self.is_user_authenticated()
 
-    def logout(self) -> None:
-        """Unbind the connection to the ldap server"""
+    def __set_ldap_arrays(self, search_filter: str) -> List[str] | None:
+        if not self.is_user_authenticated():
+            logger.warning('[__set_ldap_arrays] User is not authenticated. Skipping...')
+            return None
 
-        if self.__ldap_connection:
-            try:
-                self.__ldap_connection.unbind()
-                logger.info('[logout] Successful unbind to ldap server.')
-            except Exception as err:
-                logger.error(f'[logout] {err}')
-
-    def get_ldap_groups(self) -> List[str]:
-        """Returns A list containing the ldap groups"""
-
-        return self.__ldap_groups
-
-    def get_ldap_users(self) -> List[str]:
-        """Returns A list containing the ldap users"""
-
-        return self.__ldap_users
-
-    def __set_ldap_arrays(self, search_filter: str) -> List[str]:
         root_dn = self.ldap_config.get('root_dn', '')
 
         self.__ldap_connection.search(
@@ -186,38 +171,103 @@ class LdapService:
 
         return arr
 
-    def set_ldap_users(self) -> List[str] | None:
-        """Set the ldap users
+    def __set_all_ldap_dns(self) -> List[str] | None:
+        """Set all the ldap DN's
 
         Returns:
-            List[str] | None: Returns None if an error occurs in the process. Otherwise, returns the list of the ldap users.
+            List[str] | None: Returns `None` if an error occurs in the process. Otherwise, returns the list of the ldap DN's.
         """
 
-        if not self.is_user_authenticated():
-            logger.warning('[set_ldap_users] User is not authenticated. Skipping...')
-            return None
+        search_filter = '(objectClass=*)'
+        self.__all_ldap_dns = self.__set_ldap_arrays(search_filter=search_filter)
+
+        return self.__all_ldap_dns
+
+    def __set_all_ldap_usernames(self) -> List[str] | None:
+        """Set all the ldap users
+
+        Returns:
+            List[str] | None: Returns `None` if an error occurs in the process. Otherwise, returns the list of the ldap users.
+        """
 
         users_search_filter = '(objectClass=person)'
         users_dn = self.__set_ldap_arrays(search_filter=users_search_filter)
 
-        self.__ldap_users = [user.split('=')[1].split(',')[0] for user in users_dn]
+        if users_dn == None:
+            return None
 
-        return self.get_ldap_users()
+        self.__all_ldap_usernames = [user.split('=')[1].split(',')[0] for user in users_dn]
 
-    def set_ldap_groups(self) -> List[str] | None:
-        """Set the ldap groups
+        return self.__all_ldap_usernames
+
+    def __set_all_ldap_groups(self) -> List[str] | None:
+        """Set all the ldap groups
 
         Returns:
-            List[str] | None: Returns None if an error occurs in the process. Otherwise, returns the list of the ldap groups.
+            List[str] | None: Returns `None` if an error occurs in the process. Otherwise, returns the list of the ldap groups.
         """
-
-        if not self.is_user_authenticated():
-            logger.warning('[set_ldap_users] User is not authenticated. Skipping...')
-            return None
 
         groups_search_filter = '(objectClass=groupOfNames)'
         groups_dn = self.__set_ldap_arrays(search_filter=groups_search_filter)
 
-        self.__ldap_groups = [group.split('=')[1].split(',')[0] for group in groups_dn]
+        if groups_dn == None:
+            return None
 
-        return self.get_ldap_groups()
+        self.__all_ldap_groups = [group.split('=')[1].split(',')[0] for group in groups_dn]
+
+        return self.__all_ldap_groups
+
+    def get_all_ldap_dns(self, reset: bool = False) -> List[str] | None:
+        """Get all the ldap DN's
+
+        Args:
+            reset (bool, optional): `True` if you want to reset the list (do all the process again, including the search), `False` if you want only its value. Defaults to False.
+
+        Returns:
+            List[str] | None: Returns `None` if an error occurs in the process. Otherwise, returns a list containing all the ldap DN's.
+        """
+
+        if reset or self.__all_ldap_dns == None:
+            return self.__set_all_ldap_dns()
+
+        return self.__all_ldap_dns
+
+    def get_all_ldap_usernames(self, reset: bool = False) -> List[str] | None:
+        """Get all the ldap usernames
+
+        Args:
+            reset (bool, optional): `True` if you want to reset the list (do all the process again, including the search), `False` if you want only its value. Defaults to False.
+
+        Returns:
+            List[str] | None: Returns `None` if an error occurs in the process. Otherwise, returns a list containing all the ldap usernames.
+        """
+
+        if reset or self.__all_ldap_usernames == None:
+            return self.__set_all_ldap_usernames()
+
+        return self.__all_ldap_usernames
+
+    def get_all_ldap_groups(self, reset: bool = False) -> List[str] | None:
+        """Get all the ldap groups
+
+        Args:
+            reset (bool, optional): `True` if you want to reset the list (do all the process again, including the search), `False` if you want only its value. Defaults to False.
+
+        Returns:
+            List[str] | None: Returns `None` if an error occurs in the process. Otherwise, returns a list containing all the ldap groups.
+        """
+
+        if reset or self.__all_ldap_groups == None:
+            return self.__set_all_ldap_groups()
+
+        return self.__all_ldap_groups
+
+    def logout(self) -> None:
+        """Unbind the connection to the ldap server"""
+
+        if self.__ldap_connection:
+            try:
+                self.__ldap_connection.unbind()
+                logger.info('[logout] Successful unbind to ldap server.')
+            except Exception as err:
+                logger.error(f'[logout] {err}')
